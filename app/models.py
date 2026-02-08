1be38dc0dfd6c576f5e0b0a24d2751c61aec1839
iEkal8fGe <spid321@mail.ru>
2026-02-09 01:12:55 +0500

    Initial

--- app/models.py
+++ app/models.py
@@ -2,22 +2,28 @@ from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime
 from datetime import datetime
 from app.database import Base
 
+
 class Product(Base):
-    __tablename__ = "products"
+    __tablename__ = "product"
     
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String, nullable=True)
-    price = Column(Float)
-    quantity = Column(Integer, default=0)
+    price = Column(Float)   # 
     is_active = Column(Boolean, default=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
     
     def __repr__(self):
         return f"<Product {self.name} - ${self.price}>"
 
+
+class UserRole(Base):
+    __tablename__ = "user_role"
+
+    id = Column(Integer, primary_key=True, index=True)
+    
+
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
     
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
